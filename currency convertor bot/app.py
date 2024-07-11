ecb9362c5fb50457585b782aa3b8afed1a8b062c
PavanKumar <124707043+Pavankurapati03@users.noreply.github.com>
2024-07-11 17:28:08 +0530

    Update app.py

--- currency convertor bot/app.py
+++ currency convertor bot/app.py
@@ -3,32 +3,32 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
-
-@app.route('/convert', methods=['POST'])
+@app.route('/', methods=['POST'])
 def index():
     data = request.get_json()
     source_currency = data['queryResult']['parameters']['unit-currency']['currency']
     amount = data['queryResult']['parameters']['unit-currency']['amount']
     target_currency = data['queryResult']['parameters']['currency-name']
+    
 
-    cf = fetch_conversion_factor(source_currency, target_currency)
+
+    cf = fetch_conversion_factor(source_currency,target_currency)
     final_amount = amount * cf
-    final_amount = round(final_amount, 2)
+    final_amount = round(final_amount,2)
     response = {
-        'fulfillmentText': "{} {} is {} {}".format(amount, source_currency, final_amount, target_currency)
+        'fulfillmentText':"{} {} is {} {}".format(amount,source_currency,final_amount,target_currency)
     }
+
     return jsonify(response)
 
-def fetch_conversion_factor(source, target):
+def fetch_conversion_factor(source,target):
     url = "https://free.currconv.com/api/v7/convert?q={}_{}&compact=ultra&apiKey=9a1f3c20e42d73cd7079".format(source, target)
-
     response = requests.get(url)
     response = response.json()
 
-    return response['{}_{}'.format(source, target)]
+
+    return response['{}_{}'.format(source,target)]
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
